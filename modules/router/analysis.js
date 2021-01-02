9cd5ec3ae8de1a214229c4b84c2d9adaf4a04ed4
Pranab Lodh <pranablodh@outlook.com>
2021-01-02 18:23:15 +0530

    Junk Data API Created

--- modules/router/analysis.js
+++ modules/router/analysis.js
@@ -3,9 +3,11 @@ const router                 = express.Router();
 const analysisAuth           = require('../middleware/analysis');
 const analysisData           = require('../database/analysis/getUnprocessedData');
 const saveReport             = require('../database/analysis/saveReport');
+const markJunk               = require('../database/analysis/markJunkData');
 
 router.get('/getTestData', analysisAuth.authentication, analysisData.testDetails);
 router.post('/submitReport', analysisAuth.authentication, saveReport.submitReport);
+router.put('/markJunk', analysisAuth.authentication, markJunk.markJunk);
 
 module.exports = 
 {
